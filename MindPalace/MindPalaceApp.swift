31728bdfaa90756d96f3634e0514625de65d5ea8
Andy Cho <hello@andycho.ca>
2022-09-26 22:38:15 +0900

    Initial Commit

---
+++ MindPalace/MindPalaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindPalaceApp.swift
+//  MindPalace
+//
+//  Created by Andy Cho on 9/26/22.
+//
+
+import SwiftUI
+
+@main
+struct MindPalaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
