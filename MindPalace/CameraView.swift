0ea460b296f686e11dd34086a91a2774181caa87
Andy Cho <hello@andycho.ca>
2022-09-27 21:27:32 +0900

    Allow navigating between views

---
+++ MindPalace/CameraView.swift
@@ -0,0 +1,20 @@
+//
+//  CameraView.swift
+//  MindPalace
+//
+//  Created by Andy Cho on 9/27/22.
+//
+
+import SwiftUI
+
+struct CameraView: View {
+    var body: some View {
+        Text("Hello I am the camera view")
+    }
+}
+
+struct CameraView_Previews: PreviewProvider {
+    static var previews: some View {
+        CameraView()
+    }
+}
--- MindPalace/ContentView.swift
+++ MindPalace/ContentView.swift
@@ -7,15 +7,44 @@
 
 import SwiftUI
 
+enum NavigationDestination {
+    case CameraView
+}
+
 struct ContentView: View {
+    @State private var isShowingCameraView = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                /**
+                 * This is weird but it's not rendered
+                 * We use this to activate the segue to CameraView depending on $isShowingCameraView
+                 * which we trigger with the navigation button
+                 */
+                NavigationLink(destination: CameraView(), isActive: $isShowingCameraView) {
+                    EmptyView()
+                }
+
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundColor(.accentColor)
+                Text("Hello, world!")
+
+                Button("View") {
+                    isShowingCameraView = true
+                }
+            }
+            .padding()
+            .navigationTitle(Text("Mind Palace"))
+            .toolbar {
+                EditButton()
+            }
+            .navigationDestination(for: NavigationDestination.self) { _ in
+                // This is the only case so far
+                CameraView()
+            }
         }
-        .padding()
     }
 }
 
