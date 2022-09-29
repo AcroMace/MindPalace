897662c12f6ea370b4f22734ae32d3dc6fa34798
Andy Cho <hello@andycho.ca>
2022-09-29 23:16:47 +0900

    Replace SpriteKit with RealityKit

--- MindPalace/CameraViewController.swift
+++ MindPalace/CameraViewController.swift
@@ -6,72 +6,65 @@
 //
 
 import UIKit
-import SpriteKit
+import RealityKit
 import ARKit
 
 class CameraViewController: UIViewController {
 
-    private var sceneView = ARSKView()
-    private var scene: SKScene?
+    private var arView: ARView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.view.addSubview(sceneView)
-
-        sceneView.delegate = self
-        sceneView.showsFPS = true
-        sceneView.showsNodeCount = true
-
-        scene = SKScene(fileNamed: "Scene")
-        if let scene {
-            sceneView.presentScene(scene)
+        arView = ARView(frame: view.bounds, cameraMode: .ar, automaticallyConfigureSession: true)
+        if let arView {
+            view.addSubview(arView)
+            arView.session.delegate = self
         }
     }
 
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
-        let configuration = ARWorldTrackingConfiguration()
-        sceneView.session.run(configuration)
+        if let arView {
+            let configuration = ARWorldTrackingConfiguration()
+            arView.session.run(configuration)
+            arView.session.delegate = self
+        }
     }
 
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        sceneView.session.pause()
+        if let arView {
+            arView.session.pause()
+        }
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
 
-        sceneView.frame = self.view.bounds
+        arView?.frame = self.view.bounds
     }
 }
 
-// MARK: - ARSKViewDelegate
+// MARK: - ARSessionDelegate
 
-extension CameraViewController: ARSKViewDelegate {
+extension CameraViewController: ARSessionDelegate {
 
-    func view(_ view: ARSKView, nodeFor anchor: ARAnchor) -> SKNode? {
-        return nil
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        // no-op
     }
 
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        let alertController = UIAlertController(title: "AR session failed", message: error.localizedDescription, preferredStyle: .alert)
-        let dismissAction = UIAlertAction(title: "OK", style: .default)
-        alertController.addAction(dismissAction)
-        present(alertController, animated: true)
+    func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
+        // no-op
     }
 
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        print("SESSION INTERRUPTED")
+    func session(_ session: ARSession, didUpdate anchors: [ARAnchor]) {
+        // no-op
     }
 
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        print("SESSION INTERRUPTION ENDED")
+    func session(_ session: ARSession, didRemove anchors: [ARAnchor]) {
+        // no-op
     }
 
 }
